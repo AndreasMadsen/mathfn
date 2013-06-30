392b6a8f1f45066f69ab554c01f2765e5239d908
Andreas Madsen <amwebdk@gmail.com>
2013-06-30 13:39:01 +0200

    [space] align arguments with results

--- test/simple/erf.js
+++ test/simple/erf.js
@@ -5,15 +5,15 @@ var equals = require('../equal.js');
 
 test('testing erf function', function (t) {
   var c = [
-    [-3.0, -0.9999779095],
-    [-2.0, -0.995322265],
-    [-1.0, -0.842700792950],
-    [-0.5, -0.5204998778],
-    [ 0.0, 0.0],
-    [+0.5, 0.520499877813],
-    [+1.0, 0.8427007929],
-    [+2.0, 0.995322265],
-    [+3.0, 0.9999779095],
+    [-3.0,  -0.9999779095],
+    [-2.0,  -0.995322265],
+    [-1.0,  -0.842700792950],
+    [-0.5,  -0.5204998778],
+    [ 0.0,  0.0],
+    [+0.5,  0.520499877813],
+    [+1.0,  0.8427007929],
+    [+2.0,  0.995322265],
+    [+3.0,  0.9999779095],
   ];
 
   equals.absoluteEqual({
@@ -26,13 +26,13 @@ test('testing erf function', function (t) {
   t.end();
 });
 
-test('testing inverf function', function (t) {
+test('testing inverse erf function', function (t) {
   var c = [
-    [-1.0, -Infinity],
-    [-0.5, -0.4769362762],
-    [ 0.0, 0.0],
-    [+0.5, 0.4769362762],
-    [+1.0, +Infinity]
+    [-1.0,  -Infinity],
+    [-0.5,  -0.4769362762],
+    [ 0.0,  0.0],
+    [+0.5,  0.4769362762],
+    [+1.0,  +Infinity]
   ];
 
   equals.absoluteEqual({
@@ -45,17 +45,17 @@ test('testing inverf function', function (t) {
   t.end();
 });
 
-test('testing erfc function', function (t) {
+test('testing complementary erf function', function (t) {
   var c = [
-    [-3.0, 1.99997791],
-    [-2.0, 1.995322265],
-    [-1.0, 1.842700793],
-    [-0.5, 1.520499878],
-    [ 0.0, 1.0],
-    [+0.5, 0.4795001222],
-    [+1.0, 0.1572992071],
-    [+2.0, 0.004677734981],
-    [+3.0, 0.000022090497]
+    [-3.0,  1.99997791],
+    [-2.0,  1.995322265],
+    [-1.0,  1.842700793],
+    [-0.5,  1.520499878],
+    [ 0.0,  1.0],
+    [+0.5,  0.4795001222],
+    [+1.0,  0.1572992071],
+    [+2.0,  0.004677734981],
+    [+3.0,  0.000022090497]
   ];
 
   equals.absoluteEqual({
@@ -68,13 +68,13 @@ test('testing erfc function', function (t) {
   t.end();
 });
 
-test('testing inverfc function', function (t) {
+test('testing inverse complementary erf function', function (t) {
   var c = [
-    [0, Infinity],
-    [0.5, 0.4769362762],
-    [1.0, 0.0],
-    [1.5, -0.4769362762],
-    [2, -Infinity]
+    [0.0,   Infinity],
+    [0.5,   0.4769362762],
+    [1.0,   0.0],
+    [1.5,  -0.4769362762],
+    [2.0,  -Infinity]
   ];
 
   equals.absoluteEqual({
