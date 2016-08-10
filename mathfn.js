3de40ecd3b96e31418872f7f0e1d7a7f9e776a3a
Andreas Madsen <amwebdk@gmail.com>
2016-08-10 14:37:15 +0200

    [fix] browserify compatible

--- mathfn.js
+++ mathfn.js
@@ -1,11 +1,15 @@
 
 var files = ['erf', 'gamma', 'beta', 'log'];
 
-for (var i = 0, l = files.length; i < l; i++) {
-  var fns = require('./functions/' + files[i] + '.js');
+function exportFns(fns) {
   var keys = Object.keys(fns);
 
   for (var n = 0, r = keys.length; n < r; n++) {
     exports[ keys[n] ] = fns[keys[n]];
   }
 }
+
+exportFns(require('./functions/beta.js'));
+exportFns(require('./functions/erf.js'));
+exportFns(require('./functions/gamma.js'));
+exportFns(require('./functions/log.js'));
